89e56bf3ddf7a133b243454bc6ab5c35bf8c27a5
teohaik <teohaik@gmail.com>
2018-11-05 00:36:50 +0200

    Added intents to connect activities

--- app/src/main/java/gr/uom/adroid/lesson_06_2018_19/MainActivity.java
+++ app/src/main/java/gr/uom/adroid/lesson_06_2018_19/MainActivity.java
@@ -1,13 +1,39 @@
 package gr.uom.adroid.lesson_06_2018_19;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPlayVideo = findViewById(R.id.btnPlayVideo);
+        Button btnStartStandalone = findViewById(R.id.btnStandaloneActivity);
+
+        btnPlayVideo.setOnClickListener(this);
+        btnStartStandalone.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        if(v.getId() == R.id.btnPlayVideo){
+            intent = new Intent(this, YoutubeActivity.class);
+        }
+        else if (v.getId() == R.id.btnStandaloneActivity) {
+            intent = new Intent(this, StandaloneActivity.class);
+        }
+
+        if(intent != null){
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/gr/uom/adroid/lesson_06_2018_19/StandaloneActivity.java
@@ -0,0 +1,48 @@
+package gr.uom.adroid.lesson_06_2018_19;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+/**
+ * Created by teohaik on 5/11/2018.
+ */
+
+public class StandaloneActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standalone);
+
+        Button btnPlayVideo = findViewById(R.id.btnPlayVideo);
+        Button btnPlayPlaylist = findViewById(R.id.btnPlaylist);
+
+        btnPlayPlaylist.setOnClickListener(this);
+        btnPlayVideo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+
+        if(v.getId() == R.id.btnPlayVideo){
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, YoutubeActivity.YOUTUBE_API_KEY, YoutubeActivity.YOUTUBE_VIDEO);
+        }
+        else if(v.getId() == R.id.btnPlaylist){
+
+            intent = YouTubeStandalonePlayer.createPlaylistIntent(this, YoutubeActivity.YOUTUBE_API_KEY, YoutubeActivity.YOUTUBE_PLAYLIST);
+        }
+
+        if(intent != null){
+            startActivity(intent);
+        }
+
+    }
+
+}
